0044283ce4657aa6b11f9afb20427eb97b21a67b
aysel10 <elcin200308>
2017-09-21 20:54:37 +0400

    New Project

---
+++ app/src/main/java/com/example/ayselkas/myapplication/Activity/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.ayselkas.myapplication.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.ayselkas.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+    }
+
+}
