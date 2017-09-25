bb3d76a9b4c7579428120b916bd37484d84e42e6
aysel10 <elcin200308>
2017-09-25 11:53:23 +0400

    Registration complete. Chat starting...

--- app/src/main/java/com/example/ayselkas/myapplication/Activity/ChatActivity.java
+++ app/src/main/java/com/example/ayselkas/myapplication/Activity/ChatActivity.java
@@ -1,5 +1,7 @@
 package com.example.ayselkas.myapplication.Activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.ayselkas.myapplication.LocalStorage.Constants;
 import com.example.ayselkas.myapplication.R;
 
 public class ChatActivity extends AppCompatActivity {
@@ -26,6 +29,16 @@ public class ChatActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public static void startActivity(Context context,String receiver,String receiverUid){
+        Intent intent=new Intent(context,ChatActivity.class);
+        intent.putExtra(Constants.ARG_RECEIVER,receiver);
+        intent.putExtra(Constants.ARG_RECEIVER_UID,receiverUid);
+
+        context.startActivity(intent);
+
+
     }
 
 }
--- app/src/main/java/com/example/ayselkas/myapplication/LocalStorage/Constants.java
+++ app/src/main/java/com/example/ayselkas/myapplication/LocalStorage/Constants.java
@@ -7,4 +7,8 @@ package com.example.ayselkas.myapplication.LocalStorage;
 public class Constants {
     public static String FIREBASE_USER_TOKEN = "firebaseToken";
     public  static String FIREBASE_DB_USERS = "users";
+    public static final String ARG_RECEIVER = "receiver";
+    public static final String ARG_RECEIVER_UID = "receiver_uid";
+    public static final String ARG_CHAT_ROOMS = "chat_rooms";
+    public static final String ARG_FIREBASE_TOKEN = "firebaseToken";
 }
