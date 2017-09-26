b4a7de142c9e328606672feeb22720443e4516db
aysel10 <elcin200308>
2017-09-26 23:13:08 +0400

    ChatRecyclerAdapter

--- app/src/main/java/com/example/ayselkas/myapplication/Activity/ChatActivity.java
+++ app/src/main/java/com/example/ayselkas/myapplication/Activity/ChatActivity.java
@@ -21,14 +21,6 @@ public class ChatActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public static void startActivity(Context context,String receiver,String receiverUid){
